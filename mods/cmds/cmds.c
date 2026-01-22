d291c5de29c36149d75f35fe71fc0646fc7587ec
ohayo <9443476+ohayo@users.noreply.github.com>
2026-01-22 13:11:27 +1000

    Add .fps

--- mods/cmds/cmds.c
+++ mods/cmds/cmds.c
@@ -3,6 +3,10 @@
 #include <stdio.h>
 
 bool bSuperSpeed = false;
+bool bDrawFps = false;
+bool bNoclip = false;
+
+uintptr_t base = 0;
 
 typedef float (__cdecl* tGetSpeedValue)(float val, int flag);
 tGetSpeedValue originalGetSpeedValue = NULL;
@@ -16,11 +20,17 @@ int __cdecl hookedGetStrLen(char* str) {
     if (str != NULL && str[0] != '\0') {
         if ((uintptr_t)caller == 0x1009d9dd) {
            if (strstr(str, ".superspeed") != NULL) {   
-                printf("Superspeeeeeed!\n");
-
                 bSuperSpeed = !bSuperSpeed;
 
                 printf("Superspeed: %s\n", bSuperSpeed == 1 ? "ON" : "OFF");
+                
+                memset(str, 0, strlen(str)); 
+            }
+
+            if (strstr(str, ".fps") != NULL) {
+                bDrawFps = !bDrawFps;
+
+                printf("FPS Counter: %s\n", bDrawFps == 1 ? "ON" : "OFF");
                 
                 memset(str, 0, strlen(str)); 
             }
@@ -28,6 +38,27 @@ int __cdecl hookedGetStrLen(char* str) {
     }
 
     return originalGetStrLen(str);
+}
+
+typedef void (__cdecl* tDrawGameString)(char* arg1, float arg2, float arg3, int32_t arg4, float arg5, float arg6, int32_t arg7, int32_t arg8);
+tDrawGameString DrawGameString = NULL;
+
+typedef int32_t (__cdecl* tSub_1009d210)();
+tSub_1009d210 originalSub_1009d210 = NULL;
+
+__cdecl hookedSub_1009d210() {
+    int32_t result = originalSub_1009d210();
+
+    if (bDrawFps) {        
+        int* fpsPtr = (int*)(base + 0x1f6f1c); 
+        int currentFps = *fpsPtr;
+
+        char fpsBuffer[32];
+        sprintf(fpsBuffer, "FPS: %d", currentFps);
+        DrawGameString(fpsBuffer, -10.0f, 0.0f, 200, 5.0f, 0.0f, 7, 0);
+    }
+ 
+    return result;
 }
 
 float __cdecl hookedGetSpeedValue(float val, int flag) {
@@ -43,8 +74,11 @@ float __cdecl hookedGetSpeedValue(float val, int flag) {
 }
 
 __declspec(dllexport) void OnLoad(FacadeAPI* api) {
-    uintptr_t base = (uintptr_t)GetModuleHandleA("animEngineDLL.dll");
+    base = (uintptr_t)GetModuleHandleA("animEngineDLL.dll");
+
+    DrawGameString = (tDrawGameString)(base + 0x9e370);
 
     api->Hook((void*)(base + 0xafd30), (void*)&hookedGetSpeedValue, (void**)&originalGetSpeedValue);
     api->Hook((void*)(base + 0xb6e40), (void*)&hookedGetStrLen, (void**)&originalGetStrLen);
+    api->Hook((void*)(base + 0x9d210), (void*)&hookedSub_1009d210, (void**)&originalSub_1009d210);
 }
